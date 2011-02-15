fe4da708abcc61836d2ecafdcda289e9262498e4
Chris Barna <chris@unbrain.net>
2011-02-15 15:42:28 -0800

    Added ability to resize the image.

--- customcf/qr.js
+++ customcf/qr.js
@@ -6,6 +6,7 @@
 // Some of the base URL variables.
 var base = "http://m.scu.edu/events/?eventid=";
 var ga_campaign = "CareerCenter";
+var ga_medium = "poster";
 
 // QR Settings
 var qr_size = 200;
@@ -14,20 +15,32 @@ var base_qr = "http://chart.apis.google.com/chart?chs="+qr_size+"x"+qr_size+"&ch
 // Set event listener for the 'q' key.
 document.onkeydown = function(event) {
     if(event.keyCode == 81) {
-	url = formURL(getEventId(), "QR", "poster");
+	url = formURL(getEventId(), "QR", "ga_medium");
 	var src = base_qr+url;
 	document.getElementById("QR-code").src = src;
+	document.getElementById("QR").style.width = qr_size+"px";
 	document.getElementById("QR").style.display = 'block';
     }	    
 }
 
 function changeImage(value) {
+    ga_medium = value;
     document.getElementById("QR-code").src = base_qr+formURL(getEventId(), 'QR', value);
 }
+
+function changeImageSize(value) {
+    setBaseQR(value);
+    changeImage(ga_medium);
+    document.getElementById("QR").style.width = value+"px";
+}
+
+function setBaseQR(size) {
+    base_qr = "http://chart.apis.google.com/chart?chs="+size+"x"+size+"&cht=qr&chl=";}
 
 // Form the URL to be passed to Google with utm codes
 function formURL(eventid, source, medium) {
     var url = base+eventid+"%26utm_source="+source+"%26utm_medium="+medium+"%26=utm_campaign="+ga_campaign;
+    ga_medium = medium;
     return url;
 }
 
--- qr.cfm
+++ qr.cfm
@@ -1,9 +1,14 @@
 <script type="text/javascript" src="customcf/qr.js"></script>
-<div id="QR" style="left:200px; top:200px; width:200px;position:fixed;display:none; background-color:#fff;border:1px solid #000;padding:5px;">
+<div id="QR" style="left:200px; top:200px; position:fixed;display:none; background-color:#fff;border:1px solid #000;padding:5px;">
     <div style="float:right;"><a href="#" onclick="javascript:document.getElementById('QR').style.display = 'none';">X</a></div>
     <img src="" id="QR-code"/>
-    <select id="QR-select" onChange="javascript:changeImage(this.value)">
+    <select id="QR-select" onChange="javascript:changeImage(this.value)" style="float:left;">
 	<option value="poster">Poster</option>
 	<option value="benson">Benson Screens</option>
+    </select>
+    <select id="QR-size" onChange="javascript:changeImageSize(this.value)" style="float:left;">
+	<option value="200">200x200</option>
+	<option value="300">300x300</option>
+	<option value="400">400x400</option>
     </select>
 </div>
